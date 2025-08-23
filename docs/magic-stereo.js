a5397876fc200e5c87ba1de5008921a119dacb90
kyoyuncoding <kyohyukyun001@gmail.com>
2025-08-23 11:23:17 +0000

    Streamlining.

--- docs/magic-stereo.js
+++ docs/magic-stereo.js
@@ -1,14 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#parameters-form").onsubmit = () => {
-        let spotifyURI = document.querySelector("#song-id").value;
-        let numberOfSongs = document.querySelector("#numb-of-songs").value;
         let table = document.querySelector("#song-recommendations");
+        table.innerHTML = "";  
+        let spotifyURI = document.querySelector("#song-id").value.substring(31, 53);
         document.querySelector("#song-id").value = "";
-        document.querySelector("#numb-of-songs").value = "";
                 
-        fetch(`https://api.reccobeats.com/v1/track/recommendation?size=${numberOfSongs}&seeds=${spotifyURI}`)
+        fetch(`https://api.reccobeats.com/v1/track/recommendation?size=10&seeds=${spotifyURI}`)
         .then(response => response.json())
         .then(data => {
+
+            let trHeaderRow = document.createElement('tr');
+            let thTitleHeader = document.createElement('th');
+            thTitleHeader.textContent = "Track Recommendations Table";
+            thTitleHeader.colSpan = 3;
+            trHeaderRow.append(thTitleHeader);
+            table.append(trHeaderRow);
+
+            let trColumnNames = document.createElement('tr');
+            let thColumnNameArtist = document.createElement('th');
+            let thColumnNameSongTitle = document.createElement('th');
+            let thColumnNameSongLink = document.createElement('th');
+
+            thColumnNameArtist.textContent = "Artist(s)";
+            thColumnNameSongTitle.textContent = "Track Title"
+            thColumnNameSongLink.textContent = "Link"
+
+            trColumnNames.append(thColumnNameArtist);
+            trColumnNames.append(thColumnNameSongTitle);
+            trColumnNames.append(thColumnNameSongLink);
+            table.append(trColumnNames);
+
             // Add all the artists that are on the track as well.
             data["content"].forEach(title => {
 
