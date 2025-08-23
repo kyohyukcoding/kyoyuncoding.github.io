b55f753e004809f89eae02c567714357aa1e09e2
kyoyuncoding <kyohyukyun001@gmail.com>
2025-08-23 16:25:33 +0000

    Changing file order by type.

--- docs/magic-stereo.js
+++ docs/magic-stereo.js
@@ -14,52 +14,61 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch(`https://api.reccobeats.com/v1/track/recommendation?size=10&seeds=${spotifyURI}&acousticness=${acousticnessValue}&danceability=${danceabilityValue}&energy=${energyValue}&instrumentalness=${instrumentalnessValue}&popularity=${popularityValue}`)
         .then(response => response.json())
         .then(data => {
+            if (data["content"] !== undefined)
+            {
+                let trHeaderRow = document.createElement('tr');
+                let thTitleHeader = document.createElement('th');
+                thTitleHeader.textContent = "Track Recommendations Table";
+                thTitleHeader.colSpan = 3;
+                trHeaderRow.append(thTitleHeader);
+                table.append(trHeaderRow);
 
-            let trHeaderRow = document.createElement('tr');
-            let thTitleHeader = document.createElement('th');
-            thTitleHeader.textContent = "Track Recommendations Table";
-            thTitleHeader.colSpan = 3;
-            trHeaderRow.append(thTitleHeader);
-            table.append(trHeaderRow);
+                let trColumnNames = document.createElement('tr');
+                let thColumnNameArtist = document.createElement('th');
+                let thColumnNameSongTitle = document.createElement('th');
+                let thColumnNameSongLink = document.createElement('th');
 
-            let trColumnNames = document.createElement('tr');
-            let thColumnNameArtist = document.createElement('th');
-            let thColumnNameSongTitle = document.createElement('th');
-            let thColumnNameSongLink = document.createElement('th');
+                thColumnNameArtist.textContent = "Artist(s)";
+                thColumnNameSongTitle.textContent = "Track Title"
+                thColumnNameSongLink.textContent = "Link"
 
-            thColumnNameArtist.textContent = "Artist(s)";
-            thColumnNameSongTitle.textContent = "Track Title"
-            thColumnNameSongLink.textContent = "Link"
+                trColumnNames.append(thColumnNameArtist);
+                trColumnNames.append(thColumnNameSongTitle);
+                trColumnNames.append(thColumnNameSongLink);
+                table.append(trColumnNames);
 
-            trColumnNames.append(thColumnNameArtist);
-            trColumnNames.append(thColumnNameSongTitle);
-            trColumnNames.append(thColumnNameSongLink);
-            table.append(trColumnNames);
+                data["content"].forEach(title => {
 
-            data["content"].forEach(title => {
+                    let tr = document.createElement('tr');
 
-                let tr = document.createElement('tr');
+                    let tdArtist = document.createElement('td');
+                    tdArtist.textContent = title["artists"][0]["name"];
 
-                let tdArtist = document.createElement('td');
-                tdArtist.textContent = title["artists"][0]["name"];
+                    let tdTrackTitle = document.createElement('td');
+                    tdTrackTitle.textContent = title["trackTitle"];
+                    
+                    let tdSongLink = document.createElement('td');
+                    let songLink = document.createElement('a');
+                    songLink.href = title["href"];
+                    songLink.textContent = "Link";
+                    tdSongLink.appendChild(songLink);
 
-                let tdTrackTitle = document.createElement('td');
-                tdTrackTitle.textContent = title["trackTitle"];
-                
-                let tdSongLink = document.createElement('td');
-                let songLink = document.createElement('a');
-                songLink.href = title["href"];
-                songLink.textContent = "Link";
-                tdSongLink.appendChild(songLink);
+                    tr.appendChild(tdArtist);
+                    tr.appendChild(tdTrackTitle);
+                    tr.appendChild(tdSongLink);
 
-                tr.appendChild(tdArtist);
-                tr.appendChild(tdTrackTitle);
-                tr.appendChild(tdSongLink);
-
-                table.appendChild(tr);
-            });
-
-            document.querySelector("#song-recommendations").style.visibility = "visible";
+                    table.appendChild(tr);
+                });
+                document.querySelector("#error-messages").style.display = "none";
+                document.querySelector("#song-recommendations").style.display = "";
+            }
+            else
+            {
+                document.querySelector("#error-messages").textContent = "Invalid Spotify URL."
+            }
+        })
+        .catch(error => {
+            console.log("Error", error);
         });
         return false;
     }
